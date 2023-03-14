ae2a02ef9b4cb68dc6acbe34c46b6e719a5a8684
ReDay Zarra <redayzarra@gmail.com>
2023-03-14 16:46:12 -0400

    Adding parametric function

--- polarplane.py
+++ polarplane.py
@@ -6,3 +6,9 @@ class PolarPlane(Scene):
     e = ValueTracker(0.01)
 
     plane = PolarPlane(radius_max = 3).add_coordinates()
+    plane.shift(LEFT * 2)
+    graph1 = always_redraw(lambda:
+    ParametricFunction(lambda t: plane.polar_to_point(2 * np.sin(3*t), t),
+    t_range=[0, e.get_value()], color = GREEN)
+    )
+    dot1 = always_redraw(lab)
