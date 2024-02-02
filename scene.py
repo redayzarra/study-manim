2903b19262740b51c9b568914f13c28b56516cc6
ReDay Zarra <redayzarra@gmail.com>
2024-02-02 10:39:10 -0500

    Restarting old tutorials, finishing shifting and updaters

--- scene.py
+++ scene.py
@@ -150,6 +150,9 @@ class Graphing(Scene):
 
 
 class UpdaterGraphing(Scene):
+    def my_function(self, x: int):
+        return x**2
+
     def construct(self):
         k = ValueTracker(-4)
         axis = (
@@ -179,4 +182,67 @@ class UpdaterGraphing(Scene):
         self.add(axis, function, slope, point)
         self.wait()
         self.play(k.animate.set_value(4), run_time=3)
+        self.wait()
+
+
+class Shifting(Scene):
+    def construct(self):
+        box = Rectangle(
+            fill_color=RED_B,
+            fill_opacity=0.5,
+            stroke_opacity=0.7,
+            stroke_color=GREEN_C,
+            height=1,
+            width=1,
+        )
+
+        self.add(box)
+        self.play(box.animate.shift(RIGHT * 2), run_time=2)
+        self.play(box.animate.shift(UP * 3), run_time=2)
+        self.play(box.animate.shift(DOWN * 5 + LEFT * 5), run_time=2)
+        self.play(box.animate.shift(UP * 1.5 + RIGHT), run_time=2)
+        self.wait()
+
+
+class Fitting(Scene):
+    def construct(self):
+        axes = Axes(x_range=[-3, 3, 1], y_range=[-3, 3, 1], x_length=6, y_length=6)
+        axes.to_edge(LEFT, buff=0.5)
+
+        circle = Circle(
+            stroke_width=6, stroke_color=YELLOW, fill_color=RED_C, fill_opacity=0.8
+        )
+        circle.set_width(2).to_edge(DR)
+
+        triangle = (
+            Triangle(stroke_color=ORANGE, stroke_width=10, fill_color=GREY)
+            .set_height(2)
+            .shift(DOWN * 3 + RIGHT * 3)
+        )
+
+        self.play(Write(axes))
+        self.play(DrawBorderThenFill(circle))
+        self.play(circle.animate.set_width(1))
+        self.play(Transform(circle, triangle), run_time=3)
+        self.wait()
+
+
+class Updaters(Scene):
+    def construct(self):
+        rectangle = RoundedRectangle(
+            stroke_width=8, stroke_color=WHITE, fill_color=BLUE_B, width=4.5, height=2
+        ).shift(UP * 3 + LEFT * 4)
+        
+        text = MathTex("\\frac{3}{4} = 0.75").set_color_by_gradient(GREEN, PINK).set_height(1.5)
+        text.move_to(rectangle.get_center())
+        text.add_updater(lambda x: x.move_to(rectangle.get_center()))
+        
+        self.play(FadeIn(rectangle))
+        self.play(Write(text))
+        
+        self.play(rectangle.animate.shift(RIGHT * 1.5 + DOWN * 5), run_time = 6)
+        self.wait()
+        
+        text.clear_updaters()
+        self.play(rectangle.animate.shift(LEFT * 2 + UP), run_time = 2)
         self.wait()
