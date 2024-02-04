c3f9e9ed6728568bb5af5c27d65bc0c7b7a616a5
ReDay Zarra <redayzarra@gmail.com>
2024-02-04 10:37:05 -0500

    Finishing polar plane animation with updating values

--- scene.py
+++ scene.py
@@ -356,12 +356,66 @@ class CoordinateSystem(Scene):
         graph = plane.plot(lambda x: x**2, x_range=[-4, 4], color=GREEN)
         area = plane.get_riemann_rectangles(graph=graph, x_range=[-2, 2], dx=0.05)
 
-        axes = Axes(x_range = [-4, 4, 1], x_length = 4, y_range = [-20, 20, 5], y_length = 4).add_coordinates()
+        axes = Axes(
+            x_range=[-4, 4, 1], x_length=4, y_range=[-20, 20, 5], y_length=4
+        ).add_coordinates()
         axes.shift(RIGHT * 3 + DOWN * 1.5)
-        
-        axes_graph = axes.plot(lambda x: 2*x, x_range = [-4, 4], color = YELLOW)
-        v_lines = axes.get_vertical_lines_to_graph(graph = axes_graph, x_range = [-3, 3], num_lines = 12)
-        
+
+        axes_graph = axes.plot(lambda x: 2 * x, x_range=[-4, 4], color=YELLOW)
+        v_lines = axes.get_vertical_lines_to_graph(
+            graph=axes_graph, x_range=[-3, 3], num_lines=12
+        )
+
         self.play(Write(plane), Create(axes))
-        self.play(Create(graph), Create(axes_graph), run_time = 2)
+        self.play(Create(graph), Create(axes_graph), run_time=2)
         self.add(area, v_lines)
+
+
+class GraphingPolar(Scene):
+    def construct(self):
+        value = ValueTracker(0.01)
+
+        plane = PolarPlane(radius_max=3).add_coordinates()
+        plane.shift(LEFT * 2)
+
+        graph1 = always_redraw(
+            lambda: ParametricFunction(
+                lambda t: plane.polar_to_point(2 * np.sin(3 * t), t),
+                t_range=[0, value.get_value()],
+                color=GREEN,
+            )
+        )
+        dot1 = always_redraw(
+            lambda: Dot(fill_color=GREEN, fill_opacity=0.8)
+            .scale(0.5)
+            .move_to(graph1.get_end())
+        )
+
+        axes = Axes(
+            x_range=[0, 4, 1], x_length=3, y_range=[-3, 3, 1], y_length=3
+        ).shift(RIGHT * 4)
+        axes.add_coordinates()
+
+        graph2 = always_redraw(
+            lambda: axes.plot(
+                lambda x: 2 * np.sin(3 * x), x_range=[0, value.get_value()], color=GREEN
+            )
+        )
+        dot2 = always_redraw(
+            lambda: Dot(fill_color=GREEN, fill_opacity=0.8)
+            .scale(0.5)
+            .move_to(graph2.get_end())
+        )
+
+        title = MathTex("f(\\theta) = 2sin(3\\theta)", color=GREEN).next_to(
+            axes, UP, buff=0.2
+        )
+
+        self.play(
+            LaggedStart(
+                Write(plane), Create(axes), Write(title), run_time=3, lag_ratio=0.5
+            )
+        )
+        self.add(graph1, graph2, dot1, dot2)
+        self.play(value.animate.set_value(PI), run_time = 10, rate_func = linear)
+        self.wait()
