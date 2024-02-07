754b322b523f22037d0e5f437a44fb4d4bf63abc
ReDay Zarra <redayzarra@gmail.com>
2024-02-06 22:34:49 -0500

    Animating with vectors and moving animations around

--- scene.py
+++ scene.py
@@ -491,3 +491,72 @@ class MovingVectors(Scene):
             end=plane.coords_to_point(3, 2),
             stroke_color=YELLOW,
         ).add_tip()
+
+        vect1_name = (
+            MathTex("\\vec(v)").next_to(vect1, RIGHT, buff=0.1).set_color(YELLOW)
+        )
+
+        vect2 = Line(
+            start=plane.coords_to_point(0, 0),
+            end=plane.coords_to_point(-2, 1),
+            stroke_color=RED,
+        ).add_tip()
+        vect2_name = MathTex("\\vec(w)").next_to(vect2, LEFT, buff=0.1).set_color(RED)
+
+        vect3 = Line(
+            start=plane.coords_to_point(3, 2),
+            end=plane.coords_to_point(1, 3),
+            stroke_color=RED,
+        ).add_tip()
+
+        vect4 = Line(
+            start=plane.coords_to_point(0, 0),
+            end=plane.coords_to_point(1, 3),
+            stroke_color=GREEN,
+        ).add_tip()
+        vect4_name = (
+            MathTex("\\vec(v) +\\vec(w)")
+            .next_to(vect4, LEFT, buff=0.1)
+            .set_color(GREEN)
+        )
+
+        stuff = VGroup(
+            plane, vect1, vect1_name, vect2, vect2_name, vect3, vect4, vect4_name
+        )
+        box = RoundedRectangle(
+            height=1.5, width=1.5, corner_radius=0.1, stroke_color=PINK
+        ).to_edge(DL)
+
+        self.play(DrawBorderThenFill(plane), run_time=2)
+        self.wait()
+
+        self.play(
+            GrowFromPoint(vect1, point=vect1.get_start()), Write(vect1_name), run_time=2
+        )
+        self.wait()
+
+        self.play(
+            GrowFromPoint(vect2, point=vect2.get_start()), Write(vect2_name), run_time=2
+        )
+        self.wait()
+
+        self.play(
+            Transform(vect2, vect3),
+            vect2_name.animate.next_to(vect3, UP, buff=0.1),
+            run_time=2,
+        )
+        self.wait()
+
+        self.play(
+            LaggedStart(GrowFromPoint(vect4, point=vect4.get_start())),
+            Write(vect4_name),
+            run_time=3,
+            lag_ratio=1,
+        )
+        self.wait()
+
+        self.add(box)
+        self.wait()
+
+        self.play(stuff.animate.move_to(box.get_center()).set(width=1.2), run_time=3)
+        self.wait()
