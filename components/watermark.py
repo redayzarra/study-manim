60dd4c9e8c33829b384509827377bcefdc923164
ReDay Zarra <redayzarra@gmail.com>
2024-06-09 18:37:26 -0400

    Improving watermark

--- components/watermark.py
+++ components/watermark.py
@@ -3,5 +3,5 @@ from manim import *
 def create_watermark():
     logo = Text("StudyDSA.com").scale(0.4)
     icon = ImageMobject("icon.png").scale(0.02)
-    watermark = Group(logo, icon).arrange(LEFT, buff=0.01).to_corner(DR)
+    watermark = Group(logo, icon).arrange(LEFT, buff=0.08).to_corner(DR)
     return watermark
