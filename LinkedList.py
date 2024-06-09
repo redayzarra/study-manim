4d8d2db57ae5dc3c98c727b5cc1f431b2e8cd4ec
ReDay Zarra <redayzarra@gmail.com>
2024-06-09 17:33:40 -0400

    Refactoring code and improving layout of animations

--- LinkedList.py
+++ LinkedList.py
@@ -1,92 +1,67 @@
 from manim import *
+from SinglyLinkedList import SinglyListNode
+from watermark import create_watermark
 
-class LinkedListNode:
-    def __init__(
-        self,
-        label: str,
-        value: str,
-        radius=1.25,
-        color=WHITE,  # Color of the node
-        labelColor=WHITE,  # Label color
-        valueColor=WHITE,  # Inner value color
-    ):
-        self.node = Circle(color=color, radius=radius)
-        self.node_label = Text(label, color=labelColor).scale(0.75)
-        self.node_value = MathTex(value, color=valueColor).scale(1.5)
-
-        # Position label and value
-        self.node_label.next_to(self.node, UP)
-        self.node_value.move_to(self.node.get_center())
-
-    def create(self):
-        return VGroup(self.node, self.node_label, self.node_value)
-
-
-class LinkedListPointer(Scene):
+class AddingNodeToBeginning(Scene):
     def construct(self):
-        # Define the head linked list node
-        head = LinkedListNode("Head", "null", color=GREEN_D).create().shift(LEFT * 4)
+        self.construction()
+        self.animate_scene()
 
-        # Define the Node 1 which will connect to head
-        node1 = LinkedListNode("Node A", "1").create()
+    def construction(self):
+        """
+        Define and position the elements of the scene.
+        """
+        # Adding list nodes for the scene
+        self.head = SinglyListNode("Head", "null", color=GREEN_D).create().shift(LEFT * 4)
+        self.node1 = SinglyListNode("Node A", "1").create()
+        self.node2 = SinglyListNode("Node B", "2").create().shift(RIGHT * 4)
 
-        # Define the Node 2
-        node2 = LinkedListNode("Node B", "2").create().shift(RIGHT * 4)
+        # Adding arrows to connect list nodes
+        self.arrow = Arrow(start=self.head[0].get_right(), end=self.node2[0].get_left(), color=GREEN_D)
+        self.arrow_label = MathTex("next", color=GREEN_A).next_to(self.arrow, UP * 0.5)
 
-        # Arrow connecting head to node2
-        arrow = Arrow(start=head[0].get_right(), end=node2[0].get_left(), color=GREEN_D)
-        arrowLabel = MathTex("next", color=GREEN_A).next_to(arrow, UP * 0.5)
-
+        # Adding educational steps
         step1 = Text("1. Create new node")
         step2 = Text("2. Update head's next pointer")
         step3 = Text("3. Link new node to next node")
+        
+        # Positioning steps, need to do this in better way
+        self.steps = VGroup(step1, step2, step3).arrange(DOWN, buff=0.4).scale(0.5).shift(DOWN * 3)
+        self.steps[1].shift(RIGHT * 0.78)
+        self.steps[2].shift(RIGHT * 0.8 + UP * 0.08)
+        self.steps.to_edge(LEFT)
 
-        # Group and arrange text steps with buff
-        steps = VGroup(step1, step2, step3).arrange(DOWN, buff=0.4).scale(0.5).shift(DOWN * 3)
-        steps[1].shift(RIGHT * 0.78)
-        steps[2].shift(RIGHT * 0.8 + UP * 0.08)
-        
-        steps.to_edge(LEFT)
-        
-        logo = Text("StudyDSA.com").scale(0.4)
-        icon = ImageMobject("icon.png").scale(0.02)
-        
-        watermark = Group(logo, icon).arrange(LEFT, buff=0.01).to_corner(DR)
-
-        # Add head and its elements
-        self.add(head)
+    def animate_scene(self):
+        """
+        Add elements to the scene and animate them.
+        """
+        # Add watermark
+        watermark = create_watermark()
         self.add(watermark)
-        
 
-        # Add node2 and its elements
-        self.add(node2)
-
-        # Add the arrow
-        self.add(arrow)
-        self.play(Write(arrowLabel))
+        # Add nodes and their elements
+        self.add(self.head, self.node2, self.arrow)
+        self.play(Write(self.arrow_label))
         self.wait()
-        self.play(FadeOut(arrow), Unwrite(arrowLabel))
+        self.play(FadeOut(self.arrow), Unwrite(self.arrow_label))
 
-        # Animation to shift head and node2 and add node1
+        # Animation to shift head and node2, and add node1
         self.play(
-            head.animate.shift(LEFT),
-            node2.animate.shift(RIGHT),
-            FadeIn(node1, shift=UP, scale=0.66),
-            Write(step1),
+            self.head.animate.shift(LEFT),
+            self.node2.animate.shift(RIGHT),
+            FadeIn(self.node1, shift=UP, scale=0.66),
+            Write(self.steps[0]),
         )
         self.wait()
 
-        arrow2 = Arrow(
-            start=head[0].get_right(), end=node1[0].get_left(), color=GREEN_D
-        )
-        arrow2Label = MathTex("next", color=GREEN_A).next_to(arrow2, UP * 0.5)
-        arrow3 = Arrow(start=node1[0].get_right(), end=node2[0].get_left(), color=WHITE)
-        arrow3Label = MathTex("next").next_to(arrow3, UP * 0.5)
+        # New arrows for updated connections
+        arrow2 = Arrow(start=self.head[0].get_right(), end=self.node1[0].get_left(), color=GREEN_D)
+        arrow2_label = MathTex("next", color=GREEN_A).next_to(arrow2, UP * 0.5)
+        arrow3 = Arrow(start=self.node1[0].get_right(), end=self.node2[0].get_left(), color=WHITE)
+        arrow3_label = MathTex("next").next_to(arrow3, UP * 0.5)
 
-        self.play(GrowArrow(arrow2), Write(arrow2Label), Write(step2))
-        self.play(GrowArrow(arrow3), Write(arrow3Label), Write(step3))
+        # Display new arrows and steps
+        self.play(GrowArrow(arrow2), Write(arrow2_label), Write(self.steps[1]))
+        self.play(GrowArrow(arrow3), Write(arrow3_label), Write(self.steps[2]))
         self.wait(duration=2)
 
-
-# To run the scene, use the following command in your terminal:
-# manim -pql <script_name>.py LinkedListPointer
---
+++ watermark.py
@@ -0,0 +1,7 @@
+from manim import *
+
+def create_watermark():
+    logo = Text("StudyDSA.com").scale(0.4)
+    icon = ImageMobject("icon.png").scale(0.02)
+    watermark = Group(logo, icon).arrange(LEFT, buff=0.01).to_corner(DR)
+    return watermark
