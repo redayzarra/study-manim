5fa2cfc03b8dd81c1249dd84709dc0803495da54
ReDay Zarra <redayzarra@gmail.com>
2024-06-08 21:40:14 -0400

    Creating custom linked list node class

---
+++ LinkedList.py
@@ -0,0 +1,76 @@
+from manim import *
+
+
+class LinkedListNode:
+    def __init__(
+        self,
+        label: str,
+        value: str,
+        radius=1.25,
+        color=WHITE, # Color of the node
+        labelColor=WHITE, # Label color
+        valueColor=WHITE, # Inner value color
+    ):
+        self.node = Circle(color=color, radius=radius)
+        self.node_label = Text(label, color=labelColor).scale(0.75)
+        self.node_value = MathTex(value, color=valueColor).scale(1.5)
+
+        # Position label and value
+        self.node_label.next_to(self.node, UP)
+        self.node_value.move_to(self.node.get_center())
+
+    def create(self):
+        return VGroup(self.node, self.node_label, self.node_value)
+
+
+class LinkedListPointer(Scene):
+    def construct(self):
+        # Define the head linked list node
+        head = LinkedListNode("Head", "null", color=GREEN_D).create().shift(LEFT * 4)
+
+        # Define the Node 1 which will connect to head
+        node1 = LinkedListNode("Node1", "1").create()
+
+        # Define the Node 2
+        node2 = LinkedListNode("Node2", "2").create().shift(RIGHT * 4)
+
+        # Arrow connecting head to node2
+        arrow = Arrow(start=head[0].get_right(), end=node2[0].get_left(), color=GREEN_D)
+        arrowLabel = MathTex("next", color=GREEN_A).next_to(arrow, UP * 0.5)
+
+        step1 = Text("1. Create new node")
+
+        # Add head and its elements
+        self.add(head)
+
+        # Add node2 and its elements
+        self.add(node2)
+
+        # Add the arrow
+        self.add(arrow)
+        self.play(Write(arrowLabel))
+        self.wait()
+        self.play(FadeOut(arrow), Unwrite(arrowLabel))
+
+        # Animation to shift head and node2 and add node1
+        self.play(
+            head.animate.shift(LEFT),
+            node2.animate.shift(RIGHT),
+            FadeIn(node1, shift=UP, scale=0.66),
+        )
+        self.wait()
+
+        arrow2 = Arrow(
+            start=head[0].get_right(), end=node1[0].get_left(), color=GREEN_D
+        )
+        arrow2Label = MathTex("next", color=GREEN_D).next_to(arrow2, UP * 0.5)
+        arrow3 = Arrow(start=node1[0].get_right(), end=node2[0].get_left(), color=WHITE)
+        arrow3Label = MathTex("next").next_to(arrow3, UP * 0.5)
+
+        self.play(GrowArrow(arrow2), Write(arrow2Label))
+        self.play(GrowArrow(arrow3), Write(arrow3Label))
+        self.wait(duration=2)
+
+
+# To run the scene, use the following command in your terminal:
+# manim -pql <script_name>.py LinkedListPointer
--- scene.py
+++ scene.py
@@ -1,9 +1,9 @@
 from manim import *
 
-config.pixel_width = 1080
-config.pixel_height = 1920
-config.frame_width = 9
-config.frame_height = 16
+# config.pixel_width = 1080
+# config.pixel_height = 1920
+# config.frame_width = 9
+# config.frame_height = 16
 
 class MovingRedBox(Scene):
     def construct(self):
@@ -35,25 +35,59 @@ class MorphingTriangleGrid(Scene):
         axes = Axes(x_range=[-3, 3, 1], y_range=[-3, 3, 1], x_length=6, y_length=6)
 
         # Place the axes on the very left as starting position (with padding 0.5)
-        axes.to_edge(LEFT, buff=0.5)
+        axes.to_edge(LEFT, buff=0.2)
 
         # Create a circle with the properties you want
         circle = Circle(
             stroke_width=6, stroke_color=GREEN, fill_color=BLUE_D, fill_opacity=0.8
         )
-        # Place the circle
+        # Place the circle of width 8 near the edge (down right)
         circle.set_width(8).to_edge(DR)
 
-        triangle = (
-            Triangle(stroke_color=ORANGE, stroke_width=10, fill_color=GREY)
-            .set_height(2)
-            .shift(DOWN * 3 + RIGHT * 3)
-        )
+        # Create a triangle with the properties we want
+        triangle = Triangle(stroke_color=ORANGE, stroke_width=10, fill_color=GREY)
+        # Place the triangle of height 2 wherever we want
+        triangle.set_height(2).shift(DOWN * 3 + RIGHT * 3)
 
+        # Animate and write the axes in a pretty way
         self.play(Write(axes))
+        # Draw the circle then fill it with the color we specified earlier
         self.play(DrawBorderThenFill(circle))
+        # Animate and change the width of the circle
         self.play(circle.animate.set_width(1))
+        # Morph the circle into the triangle we defined earlier
         self.play(Transform(circle, triangle), run_time=3)
         self.wait()
 
 
+class RectangleWithText(Scene):
+    def construct(self):
+        
+        # Create and define the rectangle we want and position it
+        rectangle = RoundedRectangle(
+            stroke_width=8, stroke_color=WHITE, fill_color=BLUE_B, width=4.5, height=2
+        ).shift(UP * 3 + LEFT * 4)
+
+        # Create and define text (LaTeX)
+        text = (
+            MathTex("\\frac{3}{4} = 0.75")
+            .set_color_by_gradient(GREEN, PINK)
+            .set_height(1.5)
+        )
+        
+        # Move the text to the center of rectangle and make sure it stays
+        text.move_to(rectangle.get_center())
+        text.add_updater(lambda x: x.move_to(rectangle.get_center()))
+
+        # Animate the rectangle and write text
+        self.play(FadeIn(rectangle))
+        self.play(Write(text))
+
+        # Move the rectangle around - text stays with it
+        self.play(rectangle.animate.shift(RIGHT * 1.5 + DOWN * 5), run_time=6)
+        self.wait()
+
+        # Remove all updaters and then move rectangle - text leaves behind
+        text.clear_updaters()
+        self.play(rectangle.animate.shift(LEFT * 2 + UP), run_time=2)
+        self.wait()
