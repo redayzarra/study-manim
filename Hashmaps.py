5c6593a919c43fdb04039a45bdb78c121445b178
ReDay Zarra <redayzarra@gmail.com>
2024-06-23 22:58:32 -0400

    Hashmap visualization is set up for tomorrow

--- Hashmaps.py
+++ Hashmaps.py
@@ -25,26 +25,43 @@ class HashFunction(Scene):
         ]
         self.steps = Steps(steps).create()
 
-        # Create hashmap
-        self.buckets = VGroup()
-        for index in range(5):
+        # Create indices
+        self.indices = VGroup()
+        for index in range(4):
             index_text = Text(f"Index", font_size=25, color=GRAY_C)
             number = MathTex(f"{index}", font_size=40, color=GREEN)
-            array = Text(": []", font_size=25)
-            bucket = VGroup(index_text, number, array).arrange(
+            group = VGroup(index_text, number).arrange(
                 RIGHT, aligned_edge=DOWN, center=False, buff=0.2
             )
+            self.indices.add(group)
+
+        # Arrange the indices
+        self.indices.arrange(DOWN, aligned_edge=LEFT, buff=0.4)
+
+        # Create the buckets
+        self.buckets = VGroup()
+        for index in range(4):
+            colon = Text(":", font_size=25)
+            array = Text("[]", font_size=25)
+            bucket = VGroup(colon, array).arrange(RIGHT, buff=0.2)
+            bucket[1].shift(UP * 0.06)
             self.buckets.add(bucket)
 
         # Arrange the buckets
-        self.buckets.arrange(DOWN, aligned_edge=LEFT, buff=0.2)
+        self.buckets.arrange(DOWN, aligned_edge=LEFT, buff=0.365).next_to(
+            self.indices, RIGHT
+        )
 
         # Add braces to look like hashmap
-        self.leftBrace = Brace(self.buckets, LEFT, sharpness=1, buff=0.3)
-        self.rightBrace = Brace(self.buckets, RIGHT, sharpness=1, buff=0.3)
-        self.hashmap = VGroup(self.buckets, self.leftBrace, self.rightBrace)
-        
-        self.hashmapTitle = Text("Hashmap", font_size=35).next_to(self.hashmap, DOWN, buff=0.4)
+        self.leftBrace = Brace(self.indices, LEFT, sharpness=1)
+        self.rightBrace = Brace(self.buckets, RIGHT, sharpness=1)
+        self.hashmap = VGroup(
+            self.indices, self.buckets, self.leftBrace, self.rightBrace
+        )
+
+        self.hashmapTitle = Text("Hashmap", font_size=35).next_to(
+            self.hashmap, DOWN, buff=0.4
+        )
 
     def animate_scene(self):
         """
